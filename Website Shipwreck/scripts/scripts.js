d5a59cc4b54339b4fce8f4b9e5d2ab9be03b86be
Mandarijnpijn <97195157+Mandarijnpijn@users.noreply.github.com>
2022-01-05 21:31:50 +0100

    Add files via upload

---
+++ Website Shipwreck/scripts/scripts.js
@@ -0,0 +1,6 @@
+// this is a scrollbar --------------------------
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
